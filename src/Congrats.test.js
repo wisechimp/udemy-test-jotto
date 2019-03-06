e680ca27cfbb9eda75aff28f8d9bdc7d1fefd75d
Stuart Hicks <stuart.hicks@gmail.com>
2019-03-06 14:11:46 +0100

    Added prop-types anc check-prop-types
    Plus generic checkingProps test to TestUtils. Added to the Congrats test suite.

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import Congrats from './Congrats';
-import { findByTestAttribute } from '../test/testUtils';
+import { findByTestAttribute, checkingProps } from '../test/testUtils';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -33,4 +33,8 @@ test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttribute(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+});
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkingProps(Congrats, expectedProps);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,4 @@
+import checkPropTypes from 'check-prop-types';
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -7,4 +8,9 @@
  */
 export const findByTestAttribute = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+}
+
+export const checkingProps = (component, conformingProps) => {
+  const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
+  expect(propError).toBeUndefined();
 }
