80034b6d55cc25782f117a467e5350ff028c7a9e
Stuart Hicks <stuart.hicks@gmail.com>
2019-03-07 11:13:57 +0100

    Guessed Words component and test added
    Tests for the simple state where no words have been guessed written and satisfied.

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttribute, checkingProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{
+    guessedWord: 'train', letterMatchCount: 3
+  }],
+};
+
+/**
+ * Common function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param  {Object} props -Component props specific to this setup
+ * @return {ShallowWrapper}
+ */
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props};
+  return shallow(<GuessedWords {...setupProps} />)
+};
+
+test('does not throw warning with expected props', () => {
+  checkingProps(GuessedWords, defaultProps);
+});
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders without error', () => {
+    const component = findByTestAttribute(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttribute(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe('if there are words guessed', () => {
+
+});
