333ad409088dbdef70bdc7181acd4ab657839de7
Stuart Hicks <stuart.hicks@gmail.com>
2019-03-07 11:58:45 +0100

    Started setting up App.js
    Inserted Congrats and GuessedWords components.
    Added Bootstrap and applied some classes.
    Removed default App.css contents.

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -19,10 +19,12 @@ const GuessedWords = (props) => {
     contents = (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
-            <th>Guess</th>
-            <th>Matching Letters</th>
+        <table className="table table-sm">
+          <thead className="thead-light">
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
           </thead>
           <tbody>
             { guessedWordsRows }
