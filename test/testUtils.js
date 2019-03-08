d2a35e08dbd4c270e5bacc25043b86bff2306144
Stuart Hicks <stuart.hicks@gmail.com>
2019-03-08 14:07:47 +0100

    Redux Thunk setup
    Added Redux Thunk to the configureStore and testUtils.

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,17 +1,19 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Creating a testing store with imported reducers, middleware and initial state.
- * globals: rootReducer.
+ * globals: rootReducer, middlewares
  * @param  {onject} initialState - Initial state for the store.
  * @function storeFactory
  * @return {Store} - Redux store.
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
 
 /**
