eb5e1394c24108588844e1462ab5655b32f69b9d
Stuart Hicks <stuart.hicks@gmail.com>
2019-03-14 15:14:14 +0100

    Updates

---
+++ precache-manifest.8797e53eb15bf0595dc10010e00ff040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5d48fdd2e94f07cee4d",
+    "url": "/udemy-test-jotto/static/css/main.6a4e5d41.chunk.css"
+  },
+  {
+    "revision": "a5d48fdd2e94f07cee4d",
+    "url": "/udemy-test-jotto/static/js/main.a5d48fdd.chunk.js"
+  },
+  {
+    "revision": "b45f9a184a9999c1e5a2",
+    "url": "/udemy-test-jotto/static/js/runtime~main.b45f9a18.js"
+  },
+  {
+    "revision": "28dfe79ecc7513ea234b",
+    "url": "/udemy-test-jotto/static/js/2.28dfe79e.chunk.js"
+  },
+  {
+    "revision": "87a156a1389148529eecee966d2bb397",
+    "url": "/udemy-test-jotto/index.html"
+  }
+];
